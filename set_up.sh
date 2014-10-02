074a47122b9bd3d14d7b8bebbe6607ea064c0b3c
Brian Melton <iambmelton@gmail.com>
2014-10-02 09:53:55 -0700

    initial commit

---
+++ set_up.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# original author
+# brian melton - iambmelton@gmail.com
+
+# since october 2, 2014
+
+# TODO patch bash for Shellshock if pre-4.3
+
+# TODO Check for XCode command line tools and if they don't exist install them 
+# so that we don't have to rely on brew to do that for us.
+
+# Install brew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
+
+# Get rid of Notification Center
+launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist;
+killall NotificationCenter;
+
+# TODO Ensure that deleting these won't cause any ill-effects
+# Uninstall Safari.app
+# Uninstall Mail.app
+# Uninstall Launchpad.app
+# Uninstall iTunes.app
+
+# Remove all of the share-to-facebook type bs
+# Remove as much iCloud as possible
+
+# TODO All this
+# Uninstall Chess.app
+# Uninstall Game\ Center.app
+# Uninstall Maps.app
+# Uninstall Notes.app
+# Uninstall Photo\ Booth.app
+# Uninstall FaceTime.app
+# Uninstall Reminders.app
+# Uninstall Stickies.app
+# Uninstall TextEdit.app
+# Uninstall Contacts.app
+
+# Install Google Chrome
+# Install VLC
